64f6ba66d1d1fdadaf5a02970e9a1b48470b8d0a
hmdee <hmdeen1188@gmail.com>
2024-11-30 12:27:39 +0200

    حل اختبار علي العلاقات

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\Authenticate;
+
+
+
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function store(Request $request)
     {
+        auth()->user()->projects()->attach($request->project_id, [
+            'start_date' => $request->start_date
+        ]);
         // TASK: Add one sentence to save the project to the logged-in user
         //   by $request->project_id and with $request->start_date parameter
 
